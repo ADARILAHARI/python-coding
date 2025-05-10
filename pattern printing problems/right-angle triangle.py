ed5ad46dc30b0e7156a466833f36cc188608c49b
ADARI LAHARI PADMAVATHI <adarilahari79@gmail.com>
2025-05-10 17:57:13 +0530

    updated with more question and ans

--- pattern printing problems/right-angle triangle.py
+++ pattern printing problems/right-angle triangle.py
@@ -1,9 +1,46 @@
-'''  Half Pyramid
+'''
 *
 **
 ***
 ****
-'''
-n = 5
-for i in range(1, n + 1):
-    print("*" * i)
+
+def right_tri(n):
+    for i in range(1,n+1):
+        print('*'*i)
+           
+print(right_tri)
+
+ 
+s = 'aaabbbcccadcaa'
+print("number of a , b, c:"s.count(a,b,c))
+
+n = int(input())
+i=1
+while i<=n:
+    if i%2==0:
+        print(i)
+    i += 1
+n= int(input())
+for i in range(n,11):
+        print(f"{n}*{i}={i*n}")
+
+n= int(input())
+i=1
+while i<=10:
+      print(f'{n}*{i}={i*n}')
+      i += 1
+
+n= int(input())
+i=1
+s = 1
+while i<=n:
+    s = i*s
+    print(s)
+    i+=1
+    '''
+l1=[1,2,3,4]
+l2=[2,4,5,7]
+my_list = l1+l2
+count =  my_list.count[2,4]
+
+
